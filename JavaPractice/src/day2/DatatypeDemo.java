5601002955f20f44d506832b9781c3e2a5b1c212
sanjmore <sanjmore@INLEN8520066250.corp.capgemini.com>
2025-08-05 17:46:46 +0530

    Commit

---
+++ JavaPractice/src/day2/DatatypeDemo.java
@@ -0,0 +1,13 @@
+package day2;
+
+public class DatatypeDemo {
+
+	public static void main(String[] args) {
+		
+		int a=100, b=200;
+		System.out.println(a);
+        System.out.println(b);
+        System.out.println(a+b);
+	}
+
+}
