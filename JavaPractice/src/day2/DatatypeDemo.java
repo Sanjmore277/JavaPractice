0446cdc915ec2e100223dd59ecb6fb851a871c59
sanjmore <sanjmore@INLEN8520066250.corp.capgemini.com>
2025-08-12 15:02:44 +0530

    commit

--- JavaPractice/src/day2/DatatypeDemo.java
+++ JavaPractice/src/day2/DatatypeDemo.java
@@ -4,15 +4,21 @@ public class DatatypeDemo {
 
 	public static void main(String[] args) {
 		
-		/*int a=100, b=200;
-		System.out.println(a);
-        System.out.println(b);
-        System.out.println(a+b);*/
-        
-        int c=300, d=200;
+		int c=300, d=200;
         System.out.println(c);
         System.out.println(d);
         System.out.println(c-d);
+        
+        byte by=120;
+        System.out.println(by);
+        
+        short sh=250;
+        System.out.println(sh);
+        
+        long l=123456789;
+        System.out.println(l);
+        
+        
 	}
 
 }
